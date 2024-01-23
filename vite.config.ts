89a6037a6d351ebefdc386670e73d3672c079bc8
Matheus B. Sousa <matheusb.tec@hotmail.com>
2024-01-23 19:54:56 -0300

    Added base url

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
-import { defineConfig } from 'vite'
+import {defineConfig} from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vuetify from "vite-plugin-vuetify";
 
 export default defineConfig({
-  plugins: [
-      vue(),
-      vuetify({
-          autoImport: true,
-      }),
-  ],
+    base: process.env.NODE_ENV === 'production' ? '/gym-poker-app/' : './',
+    plugins: [
+        vue(),
+        vuetify({
+            autoImport: true,
+        }),
+    ],
 })
